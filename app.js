0c9a9cc6534f59a7850be0ff7d2fb37acd8352a8
NataGalvez <nataliac.galvezc@gmail.com>
2019-07-24 12:36:21 -0400

    se crea una constante con un objeto de movies a trabajar

--- app.js
+++
@@ -1,8 +0,0 @@
-let showMovies = (arr) => {
-  arr.forEach(element => {
-    let link = window.logic.createLink(element.id)
-    fetch(link)
-    .then((response) => {
-      return response.json();
-      console.log(link);
-    })
--- data.js
+++ data.js
@@ -1,9 +1,8 @@
 window.workApi={
 	getApi: (id)=>{
 		let api="";
-		const url="http://www.omdbapi.com/?"
-		const apiKey="apikey=b3da5f50"
-		api= url+apikey;
+		const url="http://www.omdbapi.com/?apikey=b3da5f50"
+		api= url+"&i=";+id;
 		return api;
 	}
 }
