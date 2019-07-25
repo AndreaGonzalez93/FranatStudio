465856284957969195c8d49bfad0aa83f28f88ae
NataGalvez <nataliac.galvezc@gmail.com>
2019-07-24 21:33:42 -0400

    se crea funcion de mostrar etiquetas de peliculas

--- data.js
+++ data.js
@@ -1,6 +1,6 @@
 const movie= MOVIES.movies;
 window.workApi={
-  getApi: (id)=>{
+  getApi: (id,selection)=>{
     let api="";
     const url="http://www.omdbapi.com/?apikey=b3da5f50"
     api= url+"&i="+id;
@@ -9,35 +9,57 @@ window.workApi={
       return response.json();
     })
     .then(function(myJson) {
-      let fatherDiv=document.getElementById("showHere");
-      let div=document.createElement("div");
-      div.className="carousel-item";
-      let img=document.createElement("img");
-      img.className="d-block w-100";
-      img.src=myJson.Poster;
-      fatherDiv.appendChild(div);
-      div.appendChild(img);
-
-
-      console.log(myJson);
+      if (selection=="Calification") {
+        workApi.showBanners(myJson); 
+      }
+      if (selection=="Categories") {
+        workApi.showMovies(myJson);
+      } 
   });
   },
-  showMovies: ()=>{
-    let arrMovies=[];
-   
-    for (let i = 0; i < movie.length; i++) {
-      workApi.getApi(movie[i].id);
-
-
-    }
-  },
-  postersBanners: (cali)=>{
-
+  arrByCalification: (cali)=>{
     let poster=movie.filter((movies)=> movies.calification==cali);
     for (let i = 0; i < poster.length; i++) {
-      workApi.getApi(poster[i].id);
+      workApi.getApi(poster[i].id,"Calification");
     }
+  },
+  arrByCategories: (cate)=>{
+    let poster=movie.filter((movies)=> movies.category==cate);
+    for (let i = 0; i < poster.length; i++) {
+      workApi.getApi(poster[i].id,"Categories");
+    }
+  },
+  showBanners:(myJson)=>{
+    let fatherDiv=document.getElementById("showHere");
+    let div=document.createElement("div");
+    div.className="carousel-item";
+    let img=document.createElement("img");
+    img.className="d-block w-100";
+    img.src=myJson.Poster;
+    fatherDiv.appendChild(div);
+    div.appendChild(img);
+  },
+  showMovies:(myJson)=>{
+    let fatherDiv=document.getElementById("results");
+    let div=document.createElement("div");
+    div.className="col text-center";
+    let img=document.createElement("img");
+    let p=document.createElement("p");
+    p.innerHTML=myJson.Title;
+    img.src=myJson.Poster;
+
+    div.appendChild(img);
+    div.appendChild(p);
+    fatherDiv.appendChild(div);
     
   }
 }
-workApi.postersBanners(1);
+
+workApi.arrByCalification(2);
+
+workApi.arrByCategories("fantasy");
+workApi.arrByCategories("adventure");
+workApi.arrByCategories("drama");
+workApi.arrByCategories("romance");
+workApi.arrByCategories("accion");
+workApi.arrByCategories("horror");
