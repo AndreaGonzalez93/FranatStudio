fb8ed7b369a5b97210508d2fe2a5596c0457796a
Andrea Gonzalez <gogoz.gonzalez@gmail.com>
2019-07-25 10:29:55 -0400

    arreglos de la data y eliminando codigo muerto

--- js/movies.js
+++ js/movies.js
@@ -208,17 +208,3 @@ const MOVIES = {
   ]}
 window.movies=MOVIES;
 
-const rankingMovies=[
-  'tt0111161',
-  'tt0068646',
-  'tt0071562',
-  'tt0468569', 
-  'tt0050083',
-  'tt0108052',
-  'tt0167260',
-  'tt0110912',
-  'tt0060196',
-  'tt0137523'
-];
-
-window.rankingMovies = rankingMovies;
